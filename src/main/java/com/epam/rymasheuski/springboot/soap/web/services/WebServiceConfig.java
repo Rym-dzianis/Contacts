e39c7760f32aa78cabbd09b0e9e2892acf4413c0
Dzianis <rym-dzianis@mail.ru>
2019-01-27 05:25:36 +0300

    task4

--- src/main/java/com/epam/rymasheuski/springboot/soap/web/services/WebServiceConfig.java
+++ src/main/java/com/epam/rymasheuski/springboot/soap/web/services/WebServiceConfig.java
@@ -1,40 +1,79 @@
 package com.epam.rymasheuski.springboot.soap.web.services;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+import com.epam.rymasheuski.springboot.soap.web.services.contacts.contact.ContactEndpoint;
+
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
+import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
+
 @EnableWs
 @Configuration
-public class WebServiceConfig {
+public class WebServiceConfig extends WsConfigurerAdapter {
 
-  @Bean
-  public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
-    MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
-    messageDispatcherServlet.setApplicationContext(context);
-    messageDispatcherServlet.setTransformWsdlLocations(true);
-    return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
-  }
-  
-  @Bean(name = "contacts")
-  public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema contactsSchema) {
-    DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
-    definition.setPortTypeName("ContactsPort");
-    definition.setTargetNamespace("http://services.web.soap.springboot.rymasheuski.epam.com/contacts");
-    definition.setLocationUri("/ws");
-    definition.setSchema(contactsSchema);
-    return definition;
-  }
+	@Bean
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+		MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+		messageDispatcherServlet.setApplicationContext(context);
+		messageDispatcherServlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
+	}
 
-  @Bean
-  public XsdSchema contactsSchema() {
-    return new SimpleXsdSchema(new ClassPathResource("contacts.xsd"));
-  }
+	@Bean(name = "contacts")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema contactsSchema) {
+		DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+		definition.setPortTypeName("ContactsPort");
+		definition.setTargetNamespace("http://services.web.soap.springboot.rymasheuski.epam.com/contacts");
+		definition.setLocationUri("/ws");
+		definition.setSchema(contactsSchema);
+		return definition;
+	}
+
+	@Bean
+	public XsdSchema contactsSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("contacts.xsd"));
+	}
+
+	@Bean
+	public SimplePasswordValidationCallbackHandler callbackHandler() {
+		SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
+		callbackHandler.setUsersMap(Collections.singletonMap("user", "password"));
+		return callbackHandler;
+	}
+
+	@Bean
+	public XwsSecurityInterceptor securityInterceptor() {
+		XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+		securityInterceptor.setCallbackHandler(callbackHandler());
+		securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+		return securityInterceptor;
+	}
+
+	@Bean
+	PayloadValidatingInterceptor payloadValidatingInterceptor() {
+		final PayloadValidatingInterceptor payloadValidatingInterceptor = new PayloadValidatingInterceptor();
+		payloadValidatingInterceptor.setSchema(new ClassPathResource("contacts.xsd"));
+		return payloadValidatingInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(List<EndpointInterceptor> interceptors) {
+		interceptors.add(payloadValidatingInterceptor());
+		interceptors.add(securityInterceptor());
+	}
 }
